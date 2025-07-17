3556e8c93f1ee49179a48303fb766c824e987098
MzUgM <108896003+MzUgM@users.noreply.github.com>
2025-07-16 17:42:45 -0700

    feat: add cluster command with minimist support

--- cli/cluster.js
+++ cli/cluster.js
@@ -1,2 +1,22 @@
-export default async function cli (args) {
+import minimist from 'minimist'
+import { error } from '../lib/utils.js'
+
+export const options = { command: 'cluster', strict: true }
+
+export default async function cli (argv) {
+  const args = minimist(argv, {
+    string: ['profile'],
+    alias: {
+      profile: 'p',
+    }
+  })
+  const [cmd] = args._
+
+  if (cmd === 'up') {
+  } else if (cmd === 'down') {
+  } else if (cmd === 'status') {
+  } else {
+    error(`Unknown subcommand: ${cmd}`)
+    process.exit(1)
+  }
 }
